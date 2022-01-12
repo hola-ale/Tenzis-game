581ebf00d3a0c689737f927428d081c3dd5ec177
Aleksandra Hola <homoncik.aleksandra@egmail.com>
2022-01-12 14:48:19 +0100

    generate dice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import Die from './Components/Die/Die';
+import React from 'react';
 
 export default function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+
+  function allNewDice() {
+    const newDice = []
+    for (let i=0; i<10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+  }
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
+  const diceElement = dice.map(num => <Die value={num} />)
+ 
   return (
     <main>
-      <div>Hello</div>
+      <h1>Tenzies</h1>
+      <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls. </p>
+      <div className='dice-container'>
+        {diceElement}
+      </div>
+      <button className='btn-roll' onClick={rollDice}>Roll
+      </button> 
     </main>
   );
 }
